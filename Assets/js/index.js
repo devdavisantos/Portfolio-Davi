3565c7a855f6ac93e1da9ffc0f5a6125bbdda836
Davi <davisantos2003@gmail.com>
2024-12-02 23:05:33 -0300

    Correção e incorporação area de conhecimento

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -1,3 +1,29 @@
+// Botões Next e Prev da section about
+document.addEventListener('DOMContentLoaded', function () {
+    const nextBtn = document.querySelector(".next-btn-about");
+    const prevBtn = document.querySelector(".prev-btn-about");
+    const aboutMain = document.querySelector(".about-main");
+    const aboutGeneral = document.querySelector(".about-general");
+
+    aboutMain.style.display = "block";
+    aboutGeneral.style.display = "none";
+
+    nextBtn.addEventListener("click", function () {
+        aboutMain.style.display = "none";
+        aboutGeneral.style.display = "block";
+        prevBtn.style.display = "inline-block";
+        nextBtn.style.display = "none";
+    });
+
+    prevBtn.addEventListener("click", function () {
+        aboutMain.style.display = "block";
+        aboutGeneral.style.display = "none";
+        prevBtn.style.display = "none";
+        nextBtn.style.display = "inline-block";
+    });
+});
+
+// Botões Next e Prev dos cards no project
 const containers = document.querySelectorAll(".cards-project");
 const prevBtns = document.querySelectorAll(".prev-btn");
 const nextBtns = document.querySelectorAll(".next-btn");
@@ -18,31 +44,5 @@ containers.forEach((container, index) => {
         if (container.scrollLeft > 0) {
             container.scrollLeft -= cardWidth + gap;
         }
-    });
-});
-
-// Botões Next e Prev da section about
-document.addEventListener('DOMContentLoaded', function () {
-    const nextBtn = document.querySelector(".next-btn-about");
-    const prevBtn = document.querySelector(".prev-btn-about");
-    const aboutMain = document.querySelector(".about-main");
-    const aboutGeneral = document.querySelector(".about-general");
-
-    // Inicia com a about-general oculta
-    aboutMain.style.display = "block";
-    aboutGeneral.style.display = "none";
-
-    nextBtn.addEventListener("click", function () {
-        aboutMain.style.display = "none";  // Ocultar a primeira div
-        aboutGeneral.style.display = "block";  // Exibir a segunda div
-        prevBtn.style.display = "inline-block";  // Mostrar o botão de retroceder
-        nextBtn.style.display = "none";  // Ocultar o botão de avançar
-    });
-
-    prevBtn.addEventListener("click", function () {
-        aboutMain.style.display = "block";  // Exibir a primeira div
-        aboutGeneral.style.display = "none";  // Ocultar a segunda div
-        prevBtn.style.display = "none";  // Ocultar o botão de retroceder
-        nextBtn.style.display = "inline-block";  // Exibir o botão de avançar
     });
 });
